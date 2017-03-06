3bff96efe248151d6e6f5c54fd29197f6a9fca1b
Stefano Capettini <ste.cape@gmail.com>
2017-03-06 19:10:21 +0100

    boh

---
+++ client/imports/app/pages/trends/trends-table.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+import template from './trends-table.component.html';
+ 
+@Component({
+  selector: 'trends-table',
+  template
+})
+export class TrendsTableComponent implements OnInit {
+
+  constructor () {}
+ 
+  ngOnInit() {
+
+  }
+}
